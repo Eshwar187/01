5e400d6fca9795907eb2c34f1fa105558c315df8
sarthak01745 <166209929+sarthak01745@users.noreply.github.com>
2024-10-30 19:06:39 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function showSection(sectionId) {
-    // Hide all sections
-    document.querySelectorAll('.section').forEach(section => {
-        section.classList.remove('active');
-    });
-    // Show the clicked section
-    document.getElementById(sectionId).classList.add('active');
-}
